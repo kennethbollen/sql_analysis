3c55aaad9fa9aec691331ba91c1f7442563c63b2
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-05-30 08:56:40 +0100

    changed left outer to full outer
    changed the query from a left outer join to a full outer join to find recipes with no ingredients and ingredients with no recipes

--- recipes.sql
+++ recipes.sql
@@ -1,17 +1,21 @@
--- “I need all the recipe types, and then all the recipe names and preparation instructions, and then any matching ingredient step numbers, ingredient quantities, and ingredient measurements, and finally all ingredient names from my recipes database, sorted in recipe title and step number sequence.”
--- recipe_class left join with recipes to get all the recipe types
--- recipes left join with recipe ingredients to bridge to ingredients
--- inner join ingredients with measurements to get the matching ingredients with their measurements
+-- “I need all the recipe types, and then all the recipe names and preparation instructions, and then any matching ingredient step numbers, 
+-- ingredient quantities, and ingredient measurements, and finally all ingredient names from my recipes database, 
+-- sorted in recipe title 
+-- and step number sequence.”
+-- because we need all the recipe types and all the recipe names we need a full outer join with recipe type with recipes
+-- we keep all the recipes names and do a left join with recipe inregients
+-- we're looking for the matching ingredient measurements, so we do an inner join between recipe ingredients with measurements
+-- lastly we're looking for all the ingredient names but need to keep all the recipe types and recipe names so we need another full outer join
 
-SELECT rc.RecipeClassDescription, r.RecipeTitle, r.Preparation, ri.RecipeSeqNo, ri.Amount, m.MeasurementDescription, i.IngredientName
-FROM 
+SELECT rc.RecipeClassDescription, r.RecipeTitle, r.Preparation, ri.Amount, ri.RecipeSeqNo, m.MeasurementDescription, i.IngredientName
+FROM
 (((RecipesExample.dbo.Recipe_Classes AS rc
-LEFT JOIN RecipesExample.dbo.Recipes AS r
+FULL JOIN RecipesExample.dbo.Recipes AS r
 ON rc.RecipeClassID = r.RecipeClassID)
 LEFT JOIN RecipesExample.dbo.Recipe_Ingredients AS ri
 ON ri.RecipeID = r.RecipeID)
-INNER JOIN RecipesExample.dbo.Ingredients AS i
-ON i.IngredientID = ri.IngredientID)
 INNER JOIN RecipesExample.dbo.Measurements AS m
-ON m.MeasureAmountID = i.MeasureAmountID
+ON m.MeasureAmountID = ri.MeasureAmountID)
+FULL JOIN RecipesExample.dbo.Ingredients AS i
+ON i.MeasureAmountID = m.MeasureAmountID
 ORDER BY r.RecipeTitle
