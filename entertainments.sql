356ae1c8e3592060d46fc13fde378a7990b3ba3b
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-05-31 08:52:13 +0100

    additional queries
    Show me all musical styles and the customers who prefer those styles.

--- entertainments.sql
+++ entertainments.sql
@@ -8,3 +8,16 @@ EntertainmentAgencyExample.dbo.Entertainers AS ent
 LEFT JOIN EntertainmentAgencyExample.dbo.Engagements AS eng
 ON ent.EntertainerID = eng.EntertainerID
 WHERE eng.EngagementNumber IS NULL
+
+-- “Show me all musical styles and the customers who prefer those styles.”
+-- left join musical style with musical preferances
+-- left join musical preferences with customers
+
+SELECT ms.StyleName, c.CustFirstName, c.CustLastName
+FROM
+(EntertainmentAgencyExample.dbo.Musical_Styles AS ms
+LEFT JOIN EntertainmentAgencyExample.dbo.Musical_Preferences AS mp
+ON ms.StyleID = mp.StyleID)
+LEFT JOIN EntertainmentAgencyExample.dbo.Customers AS c
+ON c.CustomerID = mp.CustomerID
+ORDER BY ms.StyleName
