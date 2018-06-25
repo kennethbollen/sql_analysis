6f305e05f5ca5a7aa4259b0bf3dd6878512ad06c
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-06-25 08:26:22 +0100

    average monthly payment type for 2015

--- orders.sql
+++ orders.sql
@@ -33,4 +33,24 @@ FROM SQLBook.dbo.Orders AS o
 WHERE YEAR(o.OrderDate) = 2015
 GROUP BY o.PaymentType
 
+-- The average price paid by card processor monthly in 2015 ranked by the average price paid for the year
+
+SELECT 
+ROW_NUMBER() OVER(ORDER BY AVG(o.TotalPrice) DESC) AS ranking,
+o.PaymentType AS ccard,
+AVG(CASE WHEN MONTH(o.OrderDate) = 1 THEN o.TotalPrice END) AS jan,
+AVG(CASE WHEN MONTH(o.OrderDate) = 2 THEN o.TotalPrice END) AS feb,
+AVG(CASE WHEN MONTH(o.OrderDate) = 3 THEN o.TotalPrice END) AS mar,
+AVG(CASE WHEN MONTH(o.OrderDate) = 4 THEN o.TotalPrice END) AS apr,
+AVG(CASE WHEN MONTH(o.OrderDate) = 5 THEN o.TotalPrice END) AS may,
+AVG(CASE WHEN MONTH(o.OrderDate) = 6 THEN o.TotalPrice END) AS jun,
+AVG(CASE WHEN MONTH(o.OrderDate) = 7 THEN o.TotalPrice END) AS jul,
+AVG(CASE WHEN MONTH(o.OrderDate) = 8 THEN o.TotalPrice END) AS aug,
+AVG(CASE WHEN MONTH(o.OrderDate) = 9 THEN o.TotalPrice END) AS sep,
+AVG(CASE WHEN MONTH(o.OrderDate) = 10 THEN o.TotalPrice END) AS oct,
+AVG(CASE WHEN MONTH(o.OrderDate) = 11 THEN o.TotalPrice END) AS nov,
+AVG(CASE WHEN MONTH(o.OrderDate) = 12 THEN o.TotalPrice END) AS decb
+FROM SQLBook.dbo.Orders AS o
+WHERE YEAR(o.OrderDate) = 2015
+GROUP BY o.PaymentType
 
